80d38145413e0799cf7cb50cfd2aa9a46dd4fe8b
nectostr <bastinda96@gmail.com>
2023-01-09 19:01:35 -0800

    repolist + minor fixes

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Githubchecker
 ### A project to automatically check repos on updates in specific date range
 
-## Inittializing on start
+## Initializing on start
 step 1: https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token
 1.1 https://github.com/settings/tokens - authorize token for UCSB organization
-TODO: check what permissions are really nessesary
+TODO: check what permissions are really necessary
 
 step 2: Make sure python 3.6+ installed. Also install requirements
--- main.py
+++ main.py
@@ -13,14 +13,15 @@ headers = {
     'X-GitHub-Api-Version': '2022-11-28',
 }
 
-start = parser.parse(config.DATE_START).replace(tzinfo=pytz.timezone('US/Pacific'))
-end = parser.parse(config.DATE_END).replace(tzinfo=pytz.timezone('US/Pacific'))
+start = parser.parse(config.DATE_START).replace(tzinfo=pytz.timezone('US/Pacific')).isoformat()
+end = parser.parse(config.DATE_END).replace(tzinfo=pytz.timezone('US/Pacific')).isoformat()
 
 print(f"{start=}, {end=}")
 
 def get_authors_branch(owner, repository_name, branch_name):
     print(f"Branch {branch_name}")
-    url_commits = f"https://api.github.com/repos/{owner}/{repository_name}/commits?sha={branch_name}"
+    url_commits = f"https://api.github.com/repos/{owner}" \
+                  f"/{repository_name}/commits?sha={branch_name}&since={start}&until={end}"
 
     r = re.get(url_commits, headers=headers)
 
@@ -42,15 +43,15 @@ def get_authors_branch(owner, repository_name, branch_name):
 
         
         print(f"What come from API {commit['committer']['date']},\n\t What I have after conv {cur}")
-        if cur >= start and cur <= end:
-            successeful_committers.add((author_email, author_nikname))
-            
-            if commit['committer']['email'] != author_email:
-                successeful_committers.add((commit['committer']['email'], commit['committer']['name']))
+        # if cur >= start and cur <= end:
+        successeful_committers.add((author_email, author_nikname))
+
+        if commit['committer']['email'] != author_email:
+            successeful_committers.add((commit['committer']['email'], commit['committer']['name']))
 
     return successeful_committers
 
-def get_authors(owner, repository_name):
+def get_authors_repo(owner, repository_name):
     """
     Get authors from single github url
 
@@ -79,35 +80,39 @@ def get_authors(owner, repository_name):
     
     return committers
 
+def get_authors_repos(owner:str="ucsb", repos_list:[list,None]=None):
+    if not repos_list:
+        repos_list = config.repos_list
+
+    authors = set()
+    for repository_name in repos_list:
+        authors.update(get_authors_repo(owner, repository_name))
+
+    return authors
+
+
 def create_table(filename: str):
     df = pd.DataFrame(columns=['email', 'GithubCheck Week 1'])
     df.to_csv(filename, index=False)
-    
-    
+
 
 def update_table(committers: set, df: pd.DataFrame, period_num=1, score=5):
-    
     emails = set(df["email"])
     for email, nickname in committers:
         if email not in emails:
             print(f"Commiter {email}, aka {nickname} not found in grades")
             continue
-        df.loc[df["email"] == email,config.column_template.format(period_num)] = score
+        df.loc[df["email"] == email, config.column_template.format(period_num)] = score
         # print(f"Added score for {email}")
 
 
 if __name__ == "__main__":
-    owner = "ucsb"
-    repository_name = "CS190B-Lab2-ckrintz"
 
-    authors = get_authors(owner, repository_name)
-    
+    authors = get_authors_repos()
     # File have to have email column for match up
     if not os.path.exists(config.filename):
         create_table(config.filename)
-    
+
     df = pd.read_csv(config.filename)
     update_table(authors, df)
     df.to_csv(config.filename, index=False)
-    
-    
