cce0d82517eed9d1e3d3f4d326def96a5c3dbf0b
nectostr <bastinda96@gmail.com>
2023-01-09 19:34:23 -0800

    automatic repo management + some minors

--- main.py
+++ main.py
@@ -16,8 +16,6 @@ headers = {
 start = parser.parse(config.DATE_START).replace(tzinfo=pytz.timezone('US/Pacific')).isoformat()
 end = parser.parse(config.DATE_END).replace(tzinfo=pytz.timezone('US/Pacific')).isoformat()
 
-print(f"{start=}, {end=}")
-
 def get_authors_branch(owner, repository_name, branch_name):
     print(f"Branch {branch_name}")
     url_commits = f"https://api.github.com/repos/{owner}" \
@@ -49,6 +47,10 @@ def get_authors_branch(owner, repository_name, branch_name):
         if commit['committer']['email'] != author_email:
             successeful_committers.add((commit['committer']['email'], commit['committer']['name']))
 
+    # move return in while to speed up process (then only first page will be checked)
+    # (since chances that some repo will be not in 100 top of updates
+    # but will be updated in last week and available for checker user
+    # are not very high for now (124 repos in total for me)
     return successeful_committers
 
 def get_authors_repo(owner, repository_name):
@@ -90,6 +92,27 @@ def get_authors_repos(owner:str="ucsb", repos_list:[list,None]=None):
 
     return authors
 
+def get_list_of_repos(user:str, template:str):
+    repos = []
+    got_repos_in_last_try = 100
+    try_num = 1
+
+    while got_repos_in_last_try == 100:
+        # if you do have non-organisation, but user - use .com/users/{user}... instead
+        url = f"https://api.github.com/orgs/{user}" \
+              f"/repos?type=all&per_page=100&sort=updated&page={try_num}"
+        try_num += 1
+
+        r = re.get(url, headers=headers)
+        if r.status_code != 200:
+            raise Exception(f"Error in getting list of repos: {r.status_code}")
+        obj = r.json()
+        # print(f"Try {try_num-1}, len of repos {len(obj)}")
+        for repo in obj:
+            if template in repo["name"].lower():
+                repos.append(repo["name"])
+        got_repos_in_last_try = len(obj)
+    return repos
 
 def create_table(filename: str):
     df = pd.DataFrame(columns=['email', 'GithubCheck Week 1'])
@@ -105,8 +128,10 @@ def update_table(committers: set, df: pd.DataFrame, period_num=1, score=5):
         df.loc[df["email"] == email, config.column_template.format(period_num)] = score
         # print(f"Added score for {email}")
 
+def example_to_run():
 
-if __name__ == "__main__":
+    if not config.repos_list:
+        config.repos_list = get_list_of_repos("ucsb", config.repos_template_name)
 
     authors = get_authors_repos()
     # File have to have email column for match up
@@ -116,3 +141,12 @@ if __name__ == "__main__":
     df = pd.read_csv(config.filename)
     update_table(authors, df)
     df.to_csv(config.filename, index=False)
+
+
+if __name__ == "__main__":
+    do_not_check_list = ["CS190B-TestLab2"]
+    repos = get_list_of_repos("ucsb", "cs190b")
+    print(repos)
+    repos = [i for i in repos if i not in do_not_check_list]
+    print(repos)
+
